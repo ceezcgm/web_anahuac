fcb1eb12bf4473a8f0c97bb35f8d8f32a538aa6f
ceezcgm <46565463+ceezcgm@users.noreply.github.com>
2019-05-18 12:54:41 -0500

    Ejercicio 8y 9
    ejercicio 8 está en el 5

---
+++ js/scrollreveal.js
@@ -0,0 +1,23 @@
+// GENERAL SETTING
+window.sr = ScrollReveal({ reset: true });
+
+// Custom Settings
+sr.reveal('#tua', { duration: 200 });
+
+sr.reveal('#tua2', { 
+  origin: 'right', 
+  duration: 2000 
+});
+
+sr.reveal('#tua3"', { 
+  rotate: { x: 100, y: 100, z: 0 },
+  duration: 1000
+});
+
+sr.reveal('#tua4', { 
+  viewFactor: 0.5
+});
+
+sr.reveal('#tua5', { 
+  duration: 200 
+});
